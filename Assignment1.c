107be71b4d0fe242fb58c01d9e3c4aeaa3317cfc
chaitanya <72591313+Trick-Ster@users.noreply.github.com>
2023-02-17 23:35:36 +0000

    Update Assignment1.c

--- Assignment1.c
+++ Assignment1.c
@@ -1,23 +1,23 @@
 
-int CP1 = 1; // pin 4 assigned to switch 1
-int CP2 = 2; // pin 6 assigned to switch 2
-int LD1 = 18;//  pin 1 will be as signal A or LED 1
-int LD2 = 19;// pin 3 will be as signal B or LED 2
-int P1 = 0;// Data read from BP1 stored in E1
-int P2 = 0;// Data read from BP2 stored in E2
-int a = 200;// First letter 'a'& width of 1st pulse
-int r = 900;// This the max value after generating C pulses used in mode for generating waveform
+int switch1 = 1; // pin 1 is assigned to switch 1 on esp32
+int switch2 = 2; // pin 2 is assigned to switch 2 on esp32
+int LED1 = 18;//  pin 1 will be as signal A or LED 1
+int LED2 = 19;// pin 3 will be as signal B or LED 2
+int A = 0;// Data read from switch1 stored in A
+int B = 0;// Data read from switch2 stored in B
+int a = 200;// First letter 'a' is the width of 1st cycle
+int r = 7500;// This the max value after generating C cycle used in mode for generating a waveform
 int b = 500;// parameter 'b' calculated as second letter*100 = 1*100 = 100
-int c = 13; // Number of pulses in the block
-int d = 4500;// Spaces btw the pulse blocks
+int c = 13; // Number of pulses in one cycle
+int d = 4500;// Space in between the pulses 
 // mode is calculated as rem(fifth letter/4) + 1 = rem(1/4) + 1 = 2. Hence we use mode 2
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(CP1, INPUT); // CP1 is an input component
-  pinMode(CP2, INPUT); // CP2 is an input component
-  pinMode(LD1, OUTPUT);// LD1 emits light used as output signal to study waveforms
-  pinMode(LD2, OUTPUT);// LD2 to set as output component 
+  pinMode(switch1, INPUT); // CP1 is an input component
+  pinMode(switch2, INPUT); // CP2 is an input component
+  pinMode(LED1, OUTPUT);// LED1 emits light used as output signal to study waveforms
+  pinMode(LED2, OUTPUT);// LED2 to set as output component 
   Serial.begin(9600);
 
 }
@@ -25,52 +25,52 @@ void setup() {
 // Loop to check the inputs and perform operations 
 void loop() {
   // put your main code here, to run repeatedly:
-  P1 = digitalRead(sw1); // Collect the P1 to know if its at LOW or High state
-  P2 = digitalRead(sw2); // summon the P2 of the second button
+  A = digitalRead(switch1); // Collect the A to know LOW or High state
+  B = digitalRead(switch2); // call the B as the second button
   
-  if(P1 == HIGH)         // using if else to 
+  if(A == HIGH)         // if else condition is used  
   {
-    digitalWrite(LD1, LOW);  // This part of the code creates the blank signal
-    digitalWrite(LD2, LOW);
+    digitalWrite(LED1, LOW);  // This part of the code creates the blank signal
+    digitalWrite(LED2, LOW);
 
   }
 
   else{
 
-    if(P2 == LOW)  
+    if(B == LOW)  
     {
-      digitalWrite(LD1, HIGH);
+      digitalWrite(LED1, HIGH);
       delay(50);
-      digitalWrite(LD2, LOW);
+      digitalWrite(LED2, LOW);
 
       for(int i =0;i<c;i++)  
       {
-        digitalWrite(LD1, HIGH);
+        digitalWrite(LED1, HIGH);
         delay(a);
-        digitalWrite(LD1, LOW);
+        digitalWrite(LED1, LOW);
         delay(b);
         a = a+50;
       }
-      digitalWrite(LD1, LOW);
+      digitalWrite(LED1, LOW);
       delay(d);  
     }
 
-    else  // This part of the code runs if switch2 is pressed
+    else  // if switch2 is pressed mode is activated
     {
-      digitalWrite(LD2, HIGH);  
+      digitalWrite(LED2, HIGH);  
       delay(50);
-      digitalWrite(LD2, LOW);
+      digitalWrite(LED2, LOW);
 
       for(int i =0; i<c;i++){   
 
-        digitalWrite(LD1, HIGH);
+        digitalWrite(LED1, HIGH);
         delay(r);
-        digitalWrite(LD1, LOW);
+        digitalWrite(LED1, LOW);
         delay(b);
         r = r-50;
       }
 
-      digitalWrite(LD1, LOW);
+      digitalWrite(LED1, LOW);
       delay(d);
 
 
